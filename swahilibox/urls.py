2c803868b73ea2c2c169316b2eefb05018aedfba
thehcker <isaacnyakoi1@gmail.com>
2018-08-25 12:35:32 +0300

    navbar responsive

--- swahilibox/urls.py
+++ swahilibox/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+#from profiles import views as profiles_views
+from contact import views as contact_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('profiles/', include('profiles.urls'))
+    path('profiles/', include('profiles.urls')),
+    path('contact/', contact_views.contact, name='contact'),
 ]
 if settings.DEBUG:
 	urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
